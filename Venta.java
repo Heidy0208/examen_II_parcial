861ff71178ffaec5f3f845bf1511f889ac6eb642
Amara Acosta <acostaamara@gmail.com>
2025-07-02 11:23:37 -0600

    examen

---
+++ Venta.java
@@ -0,0 +1,14 @@
+public class Venta {
+    private int comprador;
+    private int libroComprado;
+
+    public void mostrarResumen(){
+
+    }
+    public void calcularTotal(){
+
+    }
+    public void agregarLibro(Libro p1){
+
+    }
+}
